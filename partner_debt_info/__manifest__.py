0aa37805c962c8acc129e1d1eed0372880476ffb
Juan Pablo Garza <juanp@juanpgarza.com>
2021-06-01 22:05:51 -0300

    [13.0][ADD] partner_debt_info: creación inicial

---
+++ partner_debt_info/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 openNova - Juan Pablo Garza <juanp@opennova.com.ar>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Information about partner debt",    
+    "summary": "",
+    "version": "13.0.1.0.0",
+    "category": "Account",
+    "website": "https://github.com/OpenNovaSoft/misc-addons",
+    "author": "openNova",
+    "license": "AGPL-3",
+    "depends": ["account","partner_statement"],
+    "data": [
+        'views/res_partner_view.xml',
+        ],
+    "development_status": "Production/Stable",        
+    "installable": True,
+}
